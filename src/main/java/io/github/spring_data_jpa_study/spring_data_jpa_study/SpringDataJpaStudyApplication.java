68cece991b6cd4027d4070cb2c647c6d6c647e0a
devandref <andredepaulacontato@gmail.com>
2025-08-30 15:31:37 -0300

    init

---
+++ src/main/java/io/github/spring_data_jpa_study/spring_data_jpa_study/SpringDataJpaStudyApplication.java
@@ -0,0 +1,13 @@
+package io.github.spring_data_jpa_study.spring_data_jpa_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/spring_data_jpa_study/spring_data_jpa_study/SpringDataJpaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.spring_data_jpa_study.spring_data_jpa_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
